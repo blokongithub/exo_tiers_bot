e6f9ab3b9cf5fde57eaac37bd616141c42d82a2e
blokongithub <blokongithub@gmail.com>
2024-05-20 21:56:48 +0100

    database n stuff

---
+++ src/commands/commands.js
@@ -0,0 +1,64 @@
+const { REST, Routes, ApplicationCommandOptionType, SlashCommandBuilder, SlashCommandUserOption, SlashCommandStringOption, Options} = require('discord.js');
+const sqlite3 = require("sqlite3").verbose();
+require('dotenv').config();
+
+const db = new sqlite3.Database("database.db", sqlite3.OPEN_READWRITE,(err) => {
+    if (err) {return console.error(err.message);}
+});
+
+const commands = [{
+    name: "setteir",
+    description: "sets a users tier",
+    options: [
+        {
+            name: "target",
+            description: "the user that you are tier setting",
+            required: true,
+            type: ApplicationCommandOptionType.User
+        },
+        {
+            name: "teir",
+            description: "the teir of the user",
+            required: true,
+            type: ApplicationCommandOptionType.String,
+            choices: [
+                { name: 'bronze_1', value: 'bronze_1' },
+                { name: 'bronze_2', value: 'bronze_2' },
+                { name: 'bronze_3', value: 'bronze_3' },
+                { name: 'silver_1', value: 'silver_1' },
+                { name: 'silver_2', value: 'silver_2' },
+                { name: 'silver_3', value: 'silver_3' },
+                { name: 'gold_1', value: 'gold_1' },
+                { name: 'gold_2', value: 'gold_2' },
+                { name: 'gold_3', value: 'gold_3' },
+            ]
+            
+        }
+    ]
+    },
+    {
+        name: "tier",
+        description: "gets a users tier",
+        options: [
+            {
+                name: "target",
+                description: "the user that you are tier setting",
+                required: true,
+                type: ApplicationCommandOptionType.User
+            }]
+    }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            {body: commands}
+        );
+
+    } catch(error) {
+        console.log(error);
+    }
+})();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, IntentsBitField, EmbedBuilder, SlashCommandBuilder, GatewayIntentBits, ChannelType} = require('discord.js');
+const { Client, IntentsBitField, GatewayIntentBits} = require('discord.js');
 const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database("database.db", sqlite3.OPEN_READWRITE,(err) => {
@@ -18,12 +18,87 @@ const client = new Client({
     ]
 });
 
+function checkIfValueExists(table, column, value, callback) {
+
+    let sql = `SELECT 1 FROM ${table} WHERE ${column} = ?`;
+
+    db.get(sql, [value], (err, row) => {
+        if (err) {
+            console.error(err.message);
+            callback(err, null);
+            return;
+        }
+        
+        callback(null, row ? true : false);
+    });
+}
 client.on('ready', (c) => {
     console.log(`✅ ${c.user.tag} is online`);
     client.user.setActivity("epic pro bot")
-    new SlashCommandBuilder()
-	.setName('ping')
-	.setDescription('Replies with Pong!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isChatInputCommand()) {
+        if(interaction.commandName === "setteir") {
+            const teir = interaction.options.get("teir").value
+            const user = interaction.options.get("target").user.id
+            const guildMember = await interaction.guild.members.fetch(interaction.user.id);
+            const logchannelpriv = client.channels.cache.get('1242185431085350942');
+            const logchannelpub = client.channels.cache.get('1241815925905162301');
+            const requiredRoleId = '1241831691862147122';
+            
+            if (!guildMember.roles.cache.has(requiredRoleId)) {
+                interaction.reply({ content: 'You do not have the required role to use this command.', ephemeral: true });
+                return;
+            }
+            let sql;
+            logchannelpriv.send(`the user ${guildMember} set the tier of <@${user}> to ${teir}.`);
+            logchannelpub.send(`the user ${guildMember} set the tier of <@${user}> to ${teir}.`);
+            checkIfValueExists("tiers", "discord_id", user, (err, exists) => {
+                if (err) {
+                    console.error('Error checking value existence:', err);
+                    return;
+                }
+                
+                if (exists) {
+                    sql = "UPDATE tiers SET tier = ? WHERE discord_id = ?";
+                    db.run(sql, [teir, user], err => {
+                        if (err) {
+                            console.error(err.message);
+                            return;
+                        }
+                        interaction.reply(`Updated tier for user <@${user}> to ${teir}.`);
+                    });
+                } else {
+                    sql = "INSERT INTO tiers(discord_id, tier) VALUES (?, ?)"
+                    db.run(sql, [user, teir], err =>{
+                        if (err) {
+                            console.log(error.message);
+                        }
+                        interaction.reply(`Set tier for user <@${user}> to ${teir}.`);
+                    });
+                }
+            });
+        
+        }
+        if(interaction.commandName === "tier") {
+            const user = interaction.options.get("target").user.id;
+            let sql = "SELECT tier FROM tiers WHERE discord_id = ?";
+            db.get(sql, [user], (err, row) => {
+                if (err) {
+                    console.error(err.message);
+                    interaction.reply({ content: 'There was an error retrieving the tier.', ephemeral: true });
+                    return;
+                }
+
+                if (row) {
+                    interaction.reply({ content: `<@${user}>'s tier is ${row.tier}`, ephemeral: true });
+                } else {
+                    interaction.reply({ content: `<@${user}> does not have a tier set.`, ephemeral: true });
+                }
+            });
+        }
+    }
 });
 
 client.login(
